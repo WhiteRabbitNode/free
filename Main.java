1dbbcd066de6ffe085551e6829821a92c3a79e96
Rabbit <zbwgreen@163.com>
2022-06-10 20:21:00 +0800

    优化主方法中的测试代码

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("ABC");
         System.out.println(123456);
+    }
+    private static void patternMatchTest() {
         String str = "Hello";
         PatternMatch.which(str);
         PatternMatch.which(123);
         PatternMatch.which(2.3);
-
+    }
+    private static void recodeTest() {
         RecordTest rt = new RecordTest("abc");
         System.out.println(rt);
     }
